f86c90aaeb2f8c7ae37e767ddc6d2208cf514f61
Brad Taniguchi <bradtaniguchi@gmail.com>
2019-04-02 14:34:01 -0700

    Added ngrx-root-state

--- apps/fruit-client/src/app/app.module.ts
+++ apps/fruit-client/src/app/app.module.ts
@@ -2,6 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import {
+  APP_FEATURE_KEY,
+  initialState as appInitialState,
+  appReducer
+} from './+state/app.reducer';
+import { AppEffects } from './+state/app.effects';
+import { NxModule } from '@nrwl/nx';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { storeFreeze } from 'ngrx-store-freeze';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +28,18 @@ import { AppComponent } from './app.component';
         }
       ],
       { initialNavigation: 'enabled' }
-    )
+    ),
+    NxModule.forRoot(),
+    StoreModule.forRoot(
+      { app: appReducer },
+      {
+        initialState: { app: appInitialState },
+        metaReducers: !environment.production ? [storeFreeze] : []
+      }
+    ),
+    EffectsModule.forRoot([AppEffects]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
