2c930cd506579db87f9b3f45aeb8d964354b088e
Brad Taniguchi <bradtaniguchi@gmail.com>
2019-04-02 14:23:55 -0700

    Added base fruit-module

--- apps/fruit-client/src/app/app.module.ts
+++ apps/fruit-client/src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot(
+      [
+        {
+          path: 'fruit',
+          loadChildren: './modules/fruit/fruit.module#FruitModule'
+        }
+      ],
+      { initialNavigation: 'enabled' }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
